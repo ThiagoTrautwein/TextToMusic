b299d151d7dddb56c69bd72fff6050b266fda9e5
ThiagoTrautwein <ttrautweinsantos@gmail.com>
2023-08-25 09:28:16 -0300

    Nomes ajeitados

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 from text import Text
 
-
 print("digite um texto de entrada:\n")
 texto_entrada = input()    
 texto = Text(texto_entrada)
--- text.py
+++ text.py
@@ -7,10 +7,10 @@ from music_controler import VolumeControler
 class Text:
         
     def __init__(self, texto):
-        self.texto = texto
-        self.caractere = ''
-        self.notas = ['A','B','C','D','E','F','G']
-        self.texto_tamanho = len(texto)
+        self.text = texto
+        self.char = ''
+        self.notes = ['A','B','C','D','E','F','G']
+        self.text_length = len(texto)
         self.out = rtmidi.MidiOut()
         self.out.open_port(0)
         self.note = Note(36, self.out)
@@ -18,31 +18,31 @@ class Text:
         self.volume = VolumeControler(self.out)
 
     def GetText(self):
-        return print(self.texto)
+        return print(self.text)
 
     def GetLen(self):
-        return self.texto_tamanho
+        return self.text_length
     
     def GetCharWithLen(self, posicao):
-        return self.texto[posicao]
+        return self.text[posicao]
 
-    def PercorreTexto(self):
+    def ScanText(self):
         for i in range(0, self.GetLen()):
-            caractere_anterior = self.caractere
-            self.caractere = self.GetCharWithLen(i)
-            self.CallFunctions(caractere_anterior)
+            previous_char = self.char
+            self.char = self.GetCharWithLen(i)
+            self.CallFunctions(previous_char)
 
-    def CallFunctions(self, caractere_anterior):
-        print(self.caractere)
-        match self.caractere:
+    def CallFunctions(self, previous_char):
+        print(self.char)
+        match self.char:
             case ' ':
                 pass
                 #self.volume.StopSong()
             case 'A'|'B'|'C'|'D'|'E'|'F'|'G'|'a'|'b'|'c'|'d'|'e'|'f'|'g':
                 #se caractere = B; checar próximos 3 caracteres para ver se é 'BPM+'
-                self.note.ChangeNote(self.caractere, self.out)
+                self.note.ChangeNote(self.char, self.out)
             case 'O'|'I'|'U'|'o'|'i'|'u':
-                if caractere_anterior in ['A','B','C','D','E','F','G','a','b','c','d','e','f','g','?']:
+                if previous_char in ['A','B','C','D','E','F','G','a','b','c','d','e','f','g','?']:
                     self.note.RepeatNote(self.out)
                 else:
                     self.instrument.SetInstrument(124, self.out)
@@ -51,8 +51,8 @@ class Text:
             case '-':
                 self.volume.DefaultVolume(self.out)
             case '?':
-                caractere = self.notas[random.randrange(0,6)]
-                self.note.ChangeNote(caractere, self.out)
+                char = self.notes[random.randrange(0,6)]
+                self.note.ChangeNote(char, self.out)
             # novo case 'R' + checar se próximo caractere é '-' ou '+'
             case '\n':
                 self.instrument.SetInstrument(random.randrange(0,126), self.out)
